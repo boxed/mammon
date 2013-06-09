1bb6e8bf8f878c775fbd64ab017a4827462b558f
Anders Hovmöller <boxed@killingar.net>
2013-06-09 12:51:03 +0200

    moved some settings

--- settings.py
+++ settings.py
@@ -5,26 +5,11 @@
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'outbound.mailhop.org'
-EMAIL_HOST_USER = 'boxed'
-EMAIL_HOST_PASSWORD = ';heLvN'
-EMAIL_PORT = 2525
-SERVER_EMAIL = 'robot@eldmyra.se'
-
 SERVER_EMAIL = 'robot@eldmyra.se'
 
 ADMINS = (('Möller', 'boxed@killingar.net'),)
 
 MANAGERS = ADMINS
-
-DOCUMENT_ROOT = '/var/www-python/kodare/'
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'mammon'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = '2oVGx8VwuqUfY'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 try:
     from settings_local import *
